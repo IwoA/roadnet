c0787ce68701393d60090f15c5ae80d2634fc48b
Iwo Augustyński <iwo.augustynski@kobize.pl>
2026-03-26 13:04:56 +0100

    dodane kilka testów i wesję gdy aprs to obiekt sf.

--- R/idwgraph.R
+++ R/idwgraph.R
@@ -24,43 +24,51 @@
 idw_graph <- function(roads, aprs, power = 2) {
   library(sf)
   library(dplyr)
-
+  assertthat::assert_that(
+    "sf" %in% class(roads),
+    msg = "roads must be an sf object"
+  )
+  assertthat::assert_that(
+    "r" %in% colnames(aprs),
+    msg = "aprs must have a column 'r'"
+  )
+  assertthat::assert_that(
+    "dl_segm" %in% colnames(roads),
+    msg = "roads must have a column 'dl_segm'"
+  )
+  if (!"geometry" %in% colnames(roads)) {
+    roads <- dplyr::rename(roads, geometry = geom)
+  }
   # Trzeba usunac duplikaty drog, ktore sie roznia tylko kierunkiem
-  tictoc::tic()
   roads <- roads %>%
     mutate(centr = st_centroid(geometry)) %>%
     distinct(centr, .keep_all = TRUE) %>%
     select(-centr)
-  aprs1 <- stringr::str_trim(aprs[1, ])
-  tictoc::toc()
 
-  ## APRy do mapy
-  to_points <- function(x) {
-    lon <- as.numeric(stringr::str_split(x[1], " ")[[1]][1])
-    lat <- as.numeric(stringr::str_split(x[1], " ")[[1]][2])
-    data.frame(lon = lon, lat = lat)
+  if (!"sf" %in% class(aprs)) {
+    aprs1 <- stringr::str_trim(aprs[1, ])
+    aprs <- st_as_sf(aprs, coords = c("V1", "V2"), crs = st_crs(roads))
+    ## APRy do mapy
+    to_points <- function(x) {
+      lon <- as.numeric(stringr::str_split(x[1], " ")[[1]][1])
+      lat <- as.numeric(stringr::str_split(x[1], " ")[[1]][2])
+      data.frame(lon = lon, lat = lat)
+    }
+
+    pprs <- purrr::map(aprs1, to_points) %>% data.table::rbindlist()
+    pprs <- st_as_sf(pprs, coords = c("lon", "lat"), crs = st_crs(roads))
+    pprs$r <- as.numeric(t(aprs[2, ]))
+  } else {
+    pprs <- aprs
   }
-
-  pprs <- purrr::map(aprs1, to_points) %>% data.table::rbindlist()
-  pprs <- st_as_sf(pprs, coords = c("lon", "lat"), crs = st_crs(roads))
-  pprs$r <- as.numeric(t(aprs[2, ]))
-
-  ## test wizualny
-  # library(tmap)
-  # tmap_mode("view")
-  #
-  # tm_shape(roads) + tm_lines() + tm_shape(pprs) + tm_dots(col = "red")
-  # punkty nie pasuja do drog
 
   ## dopasowanie pprow do drog i przypisanie r
   roads$r <- NA
 
-  tictoc::tic()
   for (i in 1:nrow(pprs)) {
     points_align <- st_nearest_feature(pprs[i, ], roads)
     roads$r[points_align] <- pprs$r[i]
   }
-  tictoc::toc()
 
   #weryfikacja dopasowania
   # tm_shape(filter(roads, !is.na(r))) + tm_lines() + tm_shape(pprs) + tm_dots(col = "red")
@@ -69,7 +77,6 @@ idw_graph <- function(roads, aprs, power = 2) {
   n <- roads %>% select(r, dl_segm) %>% mutate(nodeID = c(1:n()))
 
   # Tworzenie edges miedzy nodes
-  tictoc::tic()
   e <- n %>%
     st_coordinates() %>%
     as_tibble() %>%
@@ -78,21 +85,18 @@ idw_graph <- function(roads, aprs, power = 2) {
     slice(c(1, n())) %>%
     ungroup() %>%
     mutate(start_end = rep(c('a', 'b'), times = n() / 2))
-  tictoc::toc()
+
   # Otrzymujemy dwa edges na jeden node - a i b
   # trzeba je pogrupowac wg xy wtedy jeden bedzie from a drugi to. Te bez pary trzeba wyrzucic
 
-  tictoc::tic()
   e <- e %>% mutate(xy = paste(.$X, .$Y)) %>% group_by(xy)
   e$EdgeID <- group_indices(e)
   e <- e %>% group_by(EdgeID) %>% mutate(n = n()) %>% filter(n > 1) # n = 1 oznacza slepy koniec drogi wiec  sie nie nadaje na edge
-  tictoc::toc()
 
   # Jest problem ze skrzyzowaniami
   #tm_shape(filter(n, nodeID %in% c(1, 19, 247, 327))) + tm_lines(col = "red")
 
   # Trzeba potworzyc nowe edge na zasadzie kazdy node z kazdym (expand.grid)
-  tictoc::tic()
   e2 <- filter(e, n > 2)
   e_list <- collapse::rsplit(e2, e2$EdgeID)
   krzyz <- function(x) {
@@ -110,9 +114,6 @@ idw_graph <- function(roads, aprs, power = 2) {
     tidyr::pivot_wider(values_from = nodeID, names_from = start_end)
 
   e <- rbind(e1, e2)
-  tictoc::toc()
-
-  tictoc::tic()
 
   # edges to linie wiec trzeba dodac dlugosc, ktora bedzie waga dla shortest_paths
   # ponieważ n1 to centroida odcinka to długość to suma połowy odcinków z obu stron, czyli 0.5*dl_odcinka1 + 0.5*dl_odcinka2.
@@ -128,7 +129,6 @@ idw_graph <- function(roads, aprs, power = 2) {
       waga = dl_from + dl_to # Można dodać jeszcze poprawkę na prędkość.
     ) |>
     select(EdgeID, from, to, waga)
-  tictoc::toc()
 
   # IDW z pprow wg drog ----
   # bez tidygraph: zrodla bierzemy bezposrednio z n
@@ -142,8 +142,6 @@ idw_graph <- function(roads, aprs, power = 2) {
   odl_names <- paste0("odl", seq_len(K))
 
   # Obliczenia odleglosci - macierz Dijkstra bez sf w workersach
-  tictoc::tic("Dijkstra distances")
-
   ig <- igraph::graph_from_data_frame(
     d = as_tibble(e_dist) |> dplyr::select(from, to, waga),
     directed = FALSE,
@@ -162,10 +160,7 @@ idw_graph <- function(roads, aprs, power = 2) {
   dist_mat <- t(dist_mat)
   colnames(dist_mat) <- odl_names
 
-  tictoc::toc()
-
   # Obliczanie IDW (wektorowo, bez petli)
-  tictoc::tic("IDW")
   dist_mat <- as.matrix(dist_mat)
 
   zero_mask <- dist_mat == 0
@@ -183,7 +178,6 @@ idw_graph <- function(roads, aprs, power = 2) {
 
   wynik <- n |>
     dplyr::mutate(r = dplyr::if_else(is.na(r), idw_est, r))
-  tictoc::toc()
 
   return(wynik)
 }
