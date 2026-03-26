bc1ff012267b1e39622b4c30179b72c67a46ff73
Iwo Augustyński <iwo.augustynski@kobize.pl>
2026-03-26 13:03:32 +0100

    test na danych z Legnicy

---
+++ R/legnica_test_data.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(sf)
+
+pprs <- readr::read_csv2(
+  paste0(
+    "C:\\Users\\iwo.augustynski\\Documents\\CUPT\\Data\\ITS\\Legnica\\",
+    "Sumaryczne natężenie ruchu na monitorowanych skrzyżowaniach.csv"
+  ),
+  locale = readr::locale(encoding = "ISO-8859-2")
+)
+
+pprs <- separate(pprs, col = "Lokalizacja", into = c("y", "x"), sep = ", ") |>
+  st_as_sf(coords = c("x", "y"), crs = 4326)
+
+colnames(pprs) <- c("Lokalizacja", "r", "l_poj_25", "geometry")
+
+pprs <- pprs |> mutate(r = round(r / 365))
+
+doln <- read_sf(
+  "C:\\\\Users\\iwo.augustynski\\Documents\\CUPT\\Data\\MultiNet\\polpl302_________nw.shp"
+)
+legnica <- read_sf(
+  "C:\\\\Users\\iwo.augustynski\\Documents\\CUPT\\Data\\woj_shp\\Granice\\A03_Granice_gmin.shp"
+) |>
+  filter(JPT_NAZWA_ == "Legnica") |>
+  select(JPT_NAZWA_) |>
+  st_transform(crs = st_crs(doln))
+
+test_legnica <- st_filter(doln, legnica)
+test_legnica <- test_legnica |> mutate(dl_segm = st_length(geometry))
+
+write_sf(test_legnica, "./R/test_data/legnica_test.gpkg")
+write_sf(pprs, "./R/test_data/legnica_pprs.gpkg")
--- manual_tests/load_test_data.R
+++ manual_tests/load_test_data.R
@@ -1,17 +1,13 @@
 # Manual test script: Load geospatial test data
 # This script loads geospatial data from R/test_data folder for manual testing
 
-library(sf)
-library(sfnetworks)
-library(dplyr)
-library(tidygraph)
-
 # Define path to test data
 test_data_path <- here::here("R", "test_data")
 
 # Load the shapefile
-test_data <- st_read(
-  file.path(test_data_path, "cropt_cpt3161_cpt4071_cpt3890.shp")
+test_data <- sf::st_read(
+  #file.path(test_data_path, "cropt_cpt3161_cpt4071_cpt3890.shp")
+  file.path(test_data_path, "legnica_test.gpkg")
 )
 
 # Display basic information about the loaded data
@@ -19,43 +15,29 @@ head(test_data)
 
 # Load aprs
 
-aprs <- read.csv2(
-  file.path(test_data_path, "cropt_cpt3161_cpt4071_cpt3890.coef"),
-  header = FALSE
+aprs <- sf::st_read(
+  file.path(test_data_path, "legnica_pprs.gpkg")
 )
-
-# Source n_idw function
-source(here::here("manual_tests", "n_idw.R"))
-
-
-test <- n_idw(test_data, aprs)
 
 # Source idwgraph function
 source(here::here("R", "idwgraph.R"))
 
-test_idwgraph <- idw_graph(test_data, aprs)
-
-## APRy do mapy
-aprs1 <- stringr::str_trim(aprs[1, ])
-to_points <- function(x) {
-  lon <- as.numeric(stringr::str_split_i(x, " ", 1))
-  lat <- as.numeric(stringr::str_split_i(x, " ", 2))
-  data.frame(lon = lon, lat = lat)
-}
-
-pprs <- purrr::map(aprs1, to_points) %>% purrr::list_rbind()
-pprs <- st_as_sf(pprs, coords = c("lon", "lat"), crs = st_crs(test_data))
-pprs$r <- as.numeric(t(aprs[2, ]))
-
-## NaN sa bo niektore odcinki nie lacza sie z siecia drog
+tictoc::tic("idw_graph")
+test <- idw_graph(test_data, aprs)
+tictoc::toc()
 
 #Weryfikacja
 library(tmap)
 tmap_mode("view")
-tm_shape(test_idwgraph) +
-  tm_lines(col = "r", col.scale = tm_scale_continuous()) +
+
+tm_shape(test) +
+  tm_lines(col = "r", col.scale = tm_scale_continuous(values = "viridis")) +
   tm_shape(test) +
-  tm_dots(fill = "r", size = .3) + #tm_text("r") +
-  tm_shape(pprs) +
+  tm_dots(
+    fill = "r",
+    size = .3,
+    fill.scale = tm_scale_continuous(values = "viridis")
+  ) + #tm_text("r") +
+  tm_shape(aprs) +
   tm_dots(fill = "green", size = 1) +
   tm_text("r")
